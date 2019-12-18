c610f2a541367c1d40248104ed132a732f938473
Oğuz Balkaya <balkayablky58@gmail.com>
2019-12-18 23:35:18 +0300

    extends kullanılarak güncellendi.

--- uzaylidunyalisavasi.java
+++ uzaylidunyalisavasi.java
@@ -1,79 +1,115 @@
 import java.util.Random;
 import java.util.Scanner;
 class Dunyali{
-   static String tip;
-   static int savunma;
-   static int silahGucu;
-   static int urunSayisi;
-   public static int gucHesapla(){
-       if(tip=="Çiftçi"){
-           return urunSayisi;
-       }
-       return silahGucu*savunma;
-   }
-   public Dunyali(String t, int sav, int sil, int urun){
-       tip=t;
-       savunma=sav;
-       silahGucu=sil;
-       urunSayisi=urun;
-   }
+    protected int savunma;
+    public Dunyali(int savunma){
+        this.savunma = savunma;
+    }
 }
 class Uzayli{
-    static String tip;
-    static int savunma;
-    static int lazerGucu;
-    static int madenSayisi;
-    public static int gucHesapla(){
-        if(tip=="Madenci"){
-            return madenSayisi;
-        }
+    protected int savunma;
+    public Uzayli(int savunma){
+        this.savunma=savunma;
+    }
+}
+
+class Asker extends Dunyali{
+    private int silahGucu;
+    public Asker(int sg,int sav){
+        super(sav);
+        silahGucu=sg;
+    }
+    public int gucHesapla(){
+        return silahGucu*savunma;
+    }
+}
+class Ciftci extends Dunyali{
+    private int urunSayisi;
+    public Ciftci(int us,int sav){
+        super(sav);
+        urunSayisi=us;
+    }
+
+    public int gucHesapla(){
+        return urunSayisi;
+    }
+}
+class Cyborg extends Uzayli{
+    private int lazerGucu;
+    public Cyborg(int lg,int sav){
+        super(sav);
+        lazerGucu=lg;
+    }
+
+    public int gucHesapla(){
         return lazerGucu*savunma;
     }
-    public Uzayli(String t, int sav, int laz, int maden){
-        tip=t;
-        savunma=sav;
-        lazerGucu=laz;
-        madenSayisi=maden;
+}
+class Madenci extends Uzayli{
+    private int madenSayisi;
+    public Madenci(int ms,int sav){
+        super(sav);
+        madenSayisi=ms;
+    }
+    public int gucHesapla(){
+        return savunma;
+    }
+}
+class oyun {
+    static int toplamPuan = 0;
+
+    public int oyun() {
+Dunyali [] Dunyalilar = new Dunyali[500];
+Uzayli [] Uzaylilar = new Uzayli[500];
+//Rastgele 500 Dunyali
+for(int i=0; i<500; i++){
+    Random rand = new Random();
+    if(rand.nextInt(2) == 0){
+        Dunyalilar[i] = new Asker(rand.nextInt(10),rand.nextInt(10));
+    }else{
+        Dunyalilar[i] = new Ciftci(rand.nextInt(10),rand.nextInt(10));
+    }
+}
+        for(int j=0; j<500; j++){
+            Random rand = new Random();
+            if(rand.nextInt(2) == 0){
+                Uzaylilar[j] = new Cyborg(rand.nextInt(10),rand.nextInt(10));
+            }else{
+                Uzaylilar[j] = new Madenci(rand.nextInt(10),rand.nextInt(10));
+            }
+        }
+        int dunyalilarinGucu=0,uzaylilarinGucu=0;
+        // Güçleri hesaplayalım.
+for(int a=0;a<Dunyalilar.length;a++){
+if(Dunyalilar[a] instanceof Asker){
+    dunyalilarinGucu += ((Asker)Dunyalilar[a]).gucHesapla();
+}else{
+    dunyalilarinGucu += ((Ciftci)Dunyalilar[a]).gucHesapla();
+}
+}
+
+        for(int b=0;b<Uzaylilar.length;b++){
+            if(Uzaylilar[b] instanceof Cyborg){
+                uzaylilarinGucu += ((Cyborg)Uzaylilar[b]).gucHesapla();
+            }else{
+                uzaylilarinGucu += ((Madenci)Uzaylilar[b]).gucHesapla();
+            }
+        }
+toplamPuan += dunyalilarinGucu-uzaylilarinGucu;
+return dunyalilarinGucu-uzaylilarinGucu;
     }
 }
 public class uzaylidunyalisavasi {
-    static int toplamPuan=0;
-    public static int oyun(){
-        String uzaylilar[]={"Madenci","Cyborg"};
-        String dunyalilar[]={"Çiftçi","Asker"};
-        Random rand = new Random();
-        Uzayli[] uzay = new Uzayli[500];
-        Dunyali[] dunya = new Dunyali[500];
-//Rastgele uzaylı oluşturalım.
-        for(int i=0 ; i<500; i++){
-            int rastgeletipu=rand.nextInt(2);
-            uzay[i]=new Uzayli(uzaylilar[rastgeletipu],rand.nextInt(11),rand.nextInt(11),rand.nextInt(11));
-        }
-        //Rastgele dünyalı oluşturalım.
-        for(int j=0 ; j<500; j++){
-            int rastgeletipd=rand.nextInt(2);
-            dunya[j]=new Dunyali(dunyalilar[rastgeletipd],rand.nextInt(11),rand.nextInt(11),rand.nextInt(11));
-        }
-//Toplam güçleri hesaplayalım.
-        int dunyalilarinGucu=0;
-        int uzaylilarinGucu=0;
-        for(int k=0; k<dunyalilar.length; k++){
-            dunyalilarinGucu+=dunya[k].gucHesapla();
-            uzaylilarinGucu+=uzay[k].gucHesapla();
-        }
-toplamPuan+=(dunyalilarinGucu-uzaylilarinGucu);
-        return dunyalilarinGucu-uzaylilarinGucu;
 
+    public static void main(String[] args){
+        oyun oyun = new oyun();
+        int oyunDurum=0;
+        Scanner input = new Scanner(System.in);
+        while(oyunDurum!=-1){
+            System.out.println("Puan : "+oyun.oyun());
+            oyunDurum=input.nextInt();
+            if(oyunDurum==-1) break;
+        }
+        System.out.println("Oyun bitti. Toplam puan : "+oyun.toplamPuan);
     }
-public static void main(String[] args){
-Scanner input = new Scanner(System.in);
-int oyunDurum=0;
-while(oyunDurum!=-1){
-    System.out.println("Oyunu sonlandırmak için -1 , devam ettirmek için herhangi bir sayı yaz.");
-    oyunDurum=input.nextInt();
-    if(oyunDurum==-1) break;
-    System.out.println("Bu eldeki puan : " + oyun());
 }
-
-System.out.println("Oyun bitti. Toplam Puan : " + toplamPuan);
-}}
